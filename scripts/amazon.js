be81125695b8d74a69a41a485ffdbadf8afaa5cb
L <you@example.com>
2023-11-13 15:13:10 +0700

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -74,6 +74,12 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
       });
     }
 
-    console.log(cart);
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
   });
 });
