7302f62baf1abe3c8a0964b2b8e793c0d0e031b2
L <you@example.com>
2023-11-14 20:42:32 +0700

    delete product when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -15,7 +15,9 @@ cart.forEach((cartItem) => {
     }
   });
 
-  cardSummaryHTML += `<div class="cart-item-container">
+  cardSummaryHTML += `<div class="cart-item-container js-cart-item-container-${
+    matchingProduct.id
+  }">
     <div class="delivery-date">Delivery date: Tuesday, June 21</div>
 
     <div class="cart-item-details-grid">
@@ -95,6 +97,10 @@ document.querySelectorAll(".js-delete").forEach((link) => {
   link.addEventListener("click", () => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
-    console.log(cart)
+
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    container.remove();
   });
 });
